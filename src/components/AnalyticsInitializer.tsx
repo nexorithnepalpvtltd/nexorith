7a64acbbffb65f34378502a101a30d0b3c4345d7
Rajan Sarma <148219478+RajanSharma2004@users.noreply.github.com>
2024-12-09 15:20:15 +0545

    Content

--- src/components/AnalyticsInitializer.tsx
+++ src/components/AnalyticsInitializer.tsx
@@ -1,29 +1,19 @@
-"use client";
-
-// Mark this file as a Client Component
 import { useEffect } from "react";
 
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics, logEvent } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 
-// Correct import for Firebase Analytics
+import { firebaseConfig } from "./firebaseConfig";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC40f4EjPNB8YQOe488yQrl8Z2cyFirWrs",
-  authDomain: "nexorithnepal-ac547.firebaseapp.com",
-  projectId: "nexorithnepal-ac547",
-  storageBucket: "nexorithnepal-ac547.firebasestorage.app",
-  messagingSenderId: "89262865137",
-  appId: "1:89262865137:web:245fccc6d28135a6b1c750",
-  measurementId: "G-ZVV2MXQTX1",
-};
+// Assuming you have the config
 
 export default function AnalyticsInitializer() {
   useEffect(() => {
     const app = initializeApp(firebaseConfig); // Initialize Firebase app
     if (typeof window !== "undefined" && app) {
       const analytics = getAnalytics(app); // Initialize Firebase Analytics
-      // Optionally handle analytics setup here
+      // Example of logging an event
+      logEvent(analytics, "page_view");
     }
   }, []);
 
